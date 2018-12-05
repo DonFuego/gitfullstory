c19bdc7cc69524a26fcdfa56946d5766434780ae
DonFuego <matthews.todd@gmail.com>
2018-12-05 13:49:24 -0500

    Added initial flags and commands

--- docs/scripts/install.sh
+++ docs/scripts/install.sh
@@ -5,3 +5,6 @@ echo "------------------ gitfullstory --------------------"
 echo "--------------- @author Todd Matthews --------------"
 echo "--------------- @date 07.09.2017 -------------------"
 
+# Create destination folder
+INSTALL_DIR="/usr/local/bin"
+
---
+++ gitfullstory.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"sort"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+
+	app.Name = "gitfullstory"
+	app.Usage = "command-line utility for seeing your team's open pull requests"
+	app.Version = "0.0.1"
+
+	app.Flags = []cli.Flag {
+		cli.StringFlag{
+			Name: "github_token, gt",
+			Value: "",
+			Usage: "your personal access token for github",
+			EnvVar: "GITHUB_ACCESS_TOKEN, GITHUB_TOKEN",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:    "complete",
+			Aliases: []string{"c"},
+			Usage:   "complete a task on the list",
+			Action:  func(c *cli.Context) error {
+				return nil
+			},
+		},
+		{
+			Name:    "add",
+			Aliases: []string{"a"},
+			Usage:   "add a task to the list",
+			Action:  func(c *cli.Context) error {
+				return nil
+			},
+		},
+	}
+
+	sort.Sort(cli.FlagsByName(app.Flags))
+	sort.Sort(cli.CommandsByName(app.Commands))
+
+	app.Action = func(c *cli.Context) error {
+		fmt.Println("boom! I say!")
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
