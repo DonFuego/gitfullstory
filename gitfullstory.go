6b03a8a4e91c751d2be6405edf4c8caf71438110
DonFuego <matthews.todd@gmail.com>
2018-12-06 17:16:53 -0500

    Added github integration

--- gitfullstory.go
+++ gitfullstory.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
 	"log"
 	"os"
 	"sort"
 
 	"github.com/urfave/cli"
 )
+
+func createGithubClient(accessToken string) (ctx context.Context, githubClient *github.Client) {
+	ctx = context.Background()
+	tokenSource := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: accessToken})
+	tokenClient := oauth2.NewClient(ctx, tokenSource)
+	githubClient = github.NewClient(tokenClient)
+	return
+}
 
 func main() {
 	app := cli.NewApp()
@@ -21,7 +32,7 @@ func main() {
 			Name: "github_token, gt",
 			Value: "",
 			Usage: "your personal access token for github",
-			EnvVar: "GITHUB_ACCESS_TOKEN, GITHUB_TOKEN",
+			EnvVar: "GITHUB_ACCESS_TOKEN,GITHUB_TOKEN",
 		},
 	}
 
@@ -31,6 +42,9 @@ func main() {
 			Aliases: []string{"c"},
 			Usage:   "complete a task on the list",
 			Action:  func(c *cli.Context) error {
+				if c.Bool("github_token") {
+					fmt.Println("You have a github token!")
+				}
 				return nil
 			},
 		},
@@ -39,6 +53,11 @@ func main() {
 			Aliases: []string{"a"},
 			Usage:   "add a task to the list",
 			Action:  func(c *cli.Context) error {
+				if c.Bool("github_token") {
+					fmt.Println("I'm adding stuff WITH a github token!")
+					return nil
+				}
+				fmt.Println("I'm only adding some stuff...")
 				return nil
 			},
 		},
@@ -48,7 +67,18 @@ func main() {
 	sort.Sort(cli.CommandsByName(app.Commands))
 
 	app.Action = func(c *cli.Context) error {
-		fmt.Println("boom! I say!")
+		if c.String("github_token") == "" {
+			return cli.NewExitError("Error, missing github personal access token!", 86)
+		}
+		ctx, githubClient := createGithubClient(c.String("github_token"))
+		repos, _, err := githubClient.Repositories.List(ctx, "", nil)
+		if err != nil {
+			return cli.NewExitError("Error connecting to github", 87)
+		}
+		if len(repos) > 0 {
+			fmt.Print(repos)
+		}
+
 		return nil
 	}
 
